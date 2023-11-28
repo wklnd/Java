aae21f190b7259abbbab3ddafd56ea857da0aa7e
Oscar Wiklund <147804906+OscarWiklundJonsson@users.noreply.github.com>
2023-11-28 17:31:08 +0100

    Update Simplified.java
    Added JavaDoc comments and changed countUniqueNumers method to only use Arrays.

--- Lab3/src/Simplified.java
+++ Lab3/src/Simplified.java
@@ -1,21 +1,31 @@
+package Labbar.Lab3;
+
 import java.util.*;
 
 /**
- * Detta program läser in ett godtyckligt antal heltal från användaren och beräknar medelvärdet av dessa.
- * Samt beräknar hur många av de inmatade talen som endast angivits en gång.
+ * This class provides methods to read an arbitrary number of integers from the user,
+ * calculate the mean of these numbers, and count how many of the entered numbers were unique.
  */
 class MeanUniqueNumbersSimplified {
-    // Main metoden, skapar en Array, läser in tal från användaren, beräknar medelvärdet och antalet unika tal
+    /**
+     * The main method creates an array, reads numbers from the user, calculates the mean,
+     * and counts the number of unique numbers.
+     */
     public static void main(String[] args) {
         int[] numbers = readNumbersFromUser();
         System.out.println("Medelvärde: " + calculateMean(numbers));
         System.out.println("Antal tal som endast angivits en gång: " + countUniqueNumbers(numbers));
     }
-    // Läser in heltal från användaren och returnerar en Array med dessa tal
+
+    /**
+     * Reads integers from the user and returns an array with these numbers.
+     *
+     * @return an array of integers entered by the user
+     */
     public static int[] readNumbersFromUser() {
         Scanner scanner = new Scanner(System.in); // Skapar skanner objektet
         int numberOfInts;
-        // Denna while loop kontrollerar så att det är int som skrivs in.
+        // Denna while loop kontrollerar så att det är en int som skrivs in.
         while (true) {
             System.out.print("Ange antalet heltal: ");
             try {
@@ -42,34 +52,55 @@ class MeanUniqueNumbersSimplified {
         }
         return numbers;
     }
-    // Beräknar medelvärdet av talen
+    /**
+     * Calculates the mean of the numbers in the given array.
+     *
+     * @param numbers the array of numbers to calculate the mean of
+     * @return the mean of the numbers in the array
+     */
     public static float calculateMean(int[] numbers) {
         int sum = Arrays.stream(numbers).sum(); // Lagrar alla tal i en ström och summerar dem
         return (float) sum / numbers.length; // Delar summan (sum) med antalet tal (numbers.length)
     }
 
-    // Beräknar antalet unika tal
+    /**
+     * This method counts the number of unique numbers in an array.
+     * It uses two arrays to keep track of the numbers that have been seen and the numbers that are duplicates.
+     *
+     * @param numbers the array of numbers to check for uniqueness
+     * @return the count of unique numbers in the array
+     */
     public static int countUniqueNumbers(int[] numbers) {
-        // Skapar en lista för att lagra sett unika tal
-        List<Integer> seenNumbers = new ArrayList<>();
-        // Skapar en lista för att lagra duplicerade tal
-        List<Integer> duplicateNumbers = new ArrayList<>();
+        // Array to store the numbers that have been seen
+        int[] seenNumbers = new int[numbers.length];
+        // Array to store the numbers that are duplicates
+        int[] duplicateNumbers = new int[numbers.length];
+        // Counter for the number of unique numbers seen
+        int count = 0;
+        // Counter for the number of duplicate numbers seen
+        int duplicateCount = 0;
 
-        // Loopar igenom varje tal i den givna arrayen
+        // Iterate over each number in the input array
         for (int number : numbers) {
-            // Om talet redan har setts och inte redan lagts till i duplicerade tal-listan
-            if (seenNumbers.contains(number) && !duplicateNumbers.contains(number)) {
-                // Lägger till talet i duplicerade tal-listan
-                duplicateNumbers.add(number);
+            // Flag to indicate if the current number is a duplicate
+            boolean isDuplicate = false;
+            // Check if the current number has been seen before
+            for (int seenNumber : seenNumbers) {
+                // If the number has been seen, it is a duplicate
+                if (number == seenNumber) {
+                    // Add the duplicate number to the duplicateNumbers array
+                    duplicateNumbers[duplicateCount++] = number;
+                    // Set the flag to indicate that the number is a duplicate
+                    isDuplicate = true;
+                    break;
+                }
             }
-            // Om talet inte har setts tidigare
-            else if (!seenNumbers.contains(number)) {
-                // Lägger till talet i set unika tal-listan
-                seenNumbers.add(number);
+            // If the number is not a duplicate, add it to the seenNumbers array
+            if (!isDuplicate) {
+                seenNumbers[count++] = number;
             }
         }
-        // Returnerar antalet unika tal genom att subtrahera antalet duplicerade tal från antalet sett unika tal
-        return seenNumbers.size() - duplicateNumbers.size();
+        // The number of unique numbers is the total number of numbers seen minus the number of duplicates
+        return count - duplicateCount;
     }
-    
 }
